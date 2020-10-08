25e4a5f6e8dcd5652ca32e6f3a448b939884c135
AlexSkarlatov <59266442+AlexSkarlatov@users.noreply.github.com>
2020-10-08 12:25:27 -0400

    adfasdfasdfasdf

---
+++ src/com/skarlatov/ioexample/LowerCaseInputStream.java
@@ -0,0 +1,26 @@
+package com.skarlatov.ioexample;
+
+
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class LowerCaseInputStream extends FilterInputStream {
+
+//only decorators do this super() thing so this must be adecorator on input stream
+    public LowerCaseInputStream(InputStream in){
+        super(in);
+    }
+    public int read() throws IOException{
+        int c = in.read();
+        return (c == -1 ? c : Character.toLowerCase((char) c));
+    }
+    public int read(byte[] b, int offset, int length) throws IOException{
+        int result = in.read(b, offset, length);
+
+        for(int i = offset; i < offset + result; i++)
+            b[i] = (byte) Character.toLowerCase((char)b[i]);
+
+        return result;
+    }
+}
