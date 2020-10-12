831a5d8ff9ef38255434acd1b5c4938f037c9525
AlexSkarlatov <59266442+AlexSkarlatov@users.noreply.github.com>
2020-10-11 20:43:26 -0400

    facade 3

--- src/com/skarlatov/facadework/facade/number1/aa/HomeTheaterFacade.java
+++ src/com/skarlatov/facadework/facade/number1/aa/HomeTheaterFacade.java
@@ -17,9 +17,8 @@ public class HomeTheaterFacade {
                              Projector projector,
                              Screen screen,
                              TheaterLights lights,
-                             PopcornPopper popper) {
-
-        this.amp = amp;
+                             PopcornPopper popper){
+    this.amp = amp;
         this.tuner = tuner;
         this.dvd = dvd;
         this.cd = cd;
@@ -27,15 +26,15 @@ public class HomeTheaterFacade {
         this.screen = screen;
         this.lights = lights;
         this.popper = popper;
-    }
 
-    public void watchMovie(String movie) {
-        System.out.println("Get ready to watch a movie...");
+    }
+    public void watchMovie(String movie){
+        System.out.println("watching movie soon get ready");
 
         popper.on();
         popper.pop();
 
-        lights.dim(10);
+        lights.dim(2);
         screen.down();
 
         projector.on();
@@ -44,22 +43,84 @@ public class HomeTheaterFacade {
         amp.on();
         amp.setDvd(dvd);
         amp.setSurroundSound();
-        amp.setVolume(5);
+        amp.setVolume();
 
         dvd.on();
         dvd.play(movie);
     }
 
-    public void endMovie() {
-        System.out.println("Shutting movie theater down...");
+
+    public void endMovie(){
+        System.out.println("the system is turning off");
+
         popper.off();
         lights.on();
         screen.up();
         projector.off();
-        amp.off();
         dvd.stop();
         dvd.eject();
         dvd.off();
     }
 }
 
+
+//    Amplifier amp;
+//    Tuner tuner;
+//    DvdPlayer dvd;
+//    CdPlayer cd;
+//    Projector projector;
+//    TheaterLights lights;
+//    Screen screen;
+//    PopcornPopper popper;
+//
+//    public HomeTheaterFacade(Amplifier amp,
+//                             Tuner tuner,
+//                             DvdPlayer dvd,
+//                             CdPlayer cd,
+//                             Projector projector,
+//                             Screen screen,
+//                             TheaterLights lights,
+//                             PopcornPopper popper) {
+//
+//        this.amp = amp;
+//        this.tuner = tuner;
+//        this.dvd = dvd;
+//        this.cd = cd;
+//        this.projector = projector;
+//        this.screen = screen;
+//        this.lights = lights;
+//        this.popper = popper;
+//    }
+//
+//    public void watchMovie(String movie) {
+//        System.out.println("Get ready to watch a movie...");
+//
+//        popper.on();
+//        popper.pop();
+//
+//        lights.dim(10);
+//        screen.down();
+//
+//        projector.on();
+//        projector.wideScreenMode();
+//
+//        amp.on();
+//        amp.setDvd(dvd);
+//        amp.setSurroundSound();
+//        amp.setVolume(5);
+//
+//        dvd.on();
+//        dvd.play(movie);
+//    }
+//
+//    public void endMovie() {
+//        System.out.println("Shutting movie theater down...");
+//        popper.off();
+//        lights.on();
+//        screen.up();
+//        projector.off();
+//        amp.off();
+//        dvd.stop();
+//        dvd.eject();
+//        dvd.off();
+//    }
