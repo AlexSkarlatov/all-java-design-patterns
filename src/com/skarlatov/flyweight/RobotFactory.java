37524dff33a7bcee486a8354d1d6420ad0c6936b
AlexSkarlatov <59266442+AlexSkarlatov@users.noreply.github.com>
2020-10-31 16:00:46 -0400

    this commit has made some changes to the interface hierachy of the concrete flyweight objects. I think its more conceise but more tightly coupled can be useful depending on circumstance

--- src/com/skarlatov/flyweight/RobotFactory.java
+++ src/com/skarlatov/flyweight/RobotFactory.java
@@ -14,37 +14,67 @@ public class RobotFactory {
         return robotsMap.size();
     }
 
-    public RobotInterface getRobotFromFactory(String robotCategory) throws Exception {
+    public RobotInterface getRobotFromFactory(String robotType) throws Exception {
         RobotInterface myRobot = null;
-        if ( robotsMap.containsKey(robotCategory))
+        if ( robotsMap.containsKey(robotType))
         {
-            myRobot = robotsMap.get(robotCategory);
+            myRobot = robotsMap.get(robotType);
         }
-
-
         else
+        {
+            switch (robotType)
             {
-            switch (robotCategory)
-            {
-                case "SMALL":
-                    System.out.println("We do not have a Small Robot. so we are creating a small Robot");
-                    myRobot = new SmallRobot();
-                    robotsMap.put("SMALL", myRobot);
+                case "KING":
+                    System.out.println("We do not have a KING Robot. so we are creating a KING Robot");
+                    myRobot = new Robot("KING");
+                    robotsMap.put("KING", myRobot);
                     break;
-                case "MEDIUM":
-                    System.out.println("We do not have a Medium Robot. so we are creating a medium Robot");
-                    myRobot = new MediumRobot();
-                    robotsMap.put("MEDIUM", myRobot);
+                case "QUEEN":
+                    System.out.println("We do not have a QUEEN Robot. so we are creating a QUEEN Robot");
+                    myRobot = new Robot("QUEEN");
+                    robotsMap.put("QUEEN", myRobot);
                     break;
-                case "LARGE":
-                    System.out.println("We do not have a Large Robot. so we are creating a large Robot");
-                    myRobot = new LargeRobot();
-                    robotsMap.put("LARGE", myRobot);
-                    break;
+//                case "LARGE":
+//                    System.out.println("We do not have a Large Robot. so we are creating a large Robot");
+//                    myRobot = new LargeRobot();
+//                    robotsMap.put("LARGE", myRobot);
+//                    break;
                 default:
-                    throw new Exception("Robot factory can only create small medium and large type robots");
+                    throw new Exception("Robot factory can only create [KING, QUEEN] type robots");
             }
         }
         return myRobot;
+
+
     }
 }
+
+//
+//        if ( robotsMap.containsKey(robotCategory))
+//                {
+//                myRobot = robotsMap.get(robotCategory);
+//                }
+//                else
+//                {
+//                switch (robotCategory)
+//                {
+//                case "SMALL":
+//                System.out.println("We do not have a Small Robot. so we are creating a small Robot");
+//                myRobot = new SmallRobot();
+//                robotsMap.put("SMALL", myRobot);
+//                break;
+//                case "MEDIUM":
+//                System.out.println("We do not have a Medium Robot. so we are creating a medium Robot");
+//                myRobot = new MediumRobot();
+//                robotsMap.put("MEDIUM", myRobot);
+//                break;
+//                case "LARGE":
+//                System.out.println("We do not have a Large Robot. so we are creating a large Robot");
+//                myRobot = new LargeRobot();
+//                robotsMap.put("LARGE", myRobot);
+//                break;
+//default:
+//        throw new Exception("Robot factory can only create small medium and large type robots");
+//        }
+//        }
+//        return myRobot;
