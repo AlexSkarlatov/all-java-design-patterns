1d18390b68ae769b2d769fe6a072d3ab5cd11a0e
AlexSkarlatov <59266442+AlexSkarlatov@users.noreply.github.com>
2020-12-02 17:03:02 -0500

    creating client script

--- src/com/skarlatov/memento_pattern/challenge/Client.java
+++ src/com/skarlatov/memento_pattern/challenge/Client.java
@@ -6,7 +6,16 @@ public class Client {
         Caretaker caretaker = new Caretaker();
 
 
+
+        originator.setState("state 1");
+        originator.setState("state 2");
+        caretaker.add(originator.saveStateToMemento());
+        originator.setState("state 3");
+
         caretaker.add(originator.saveStateToMemento()) ;
+        originator.setState("state 4");
+        System.out.println("Current state:  " + originator.getState());
+        //hte only thing that is allowed to create a memento is the originator itself
     }
 
 }
