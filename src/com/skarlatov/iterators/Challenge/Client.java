9e518da60384bb72e1f28fb05777a4e9a5d2dbec
AlexSkarlatov <59266442+AlexSkarlatov@users.noreply.github.com>
2020-12-28 21:26:55 -0500

    challenge for iterator completed

--- src/com/skarlatov/iterators/Challenge/Client.java
+++ src/com/skarlatov/iterators/Challenge/Client.java
@@ -1,9 +1,30 @@
 package com.skarlatov.iterators.Challenge;
 
+import com.skarlatov.iterators.Iterator;
+
 public class Client {
 
+    public static void print(IteratorInterface ii){
+        while(!ii.isDone()){
+            System.out.println(ii.next());
+        }
+    }
     public static void main(String[] args){
 
+
+        System.out.println("***Iterator Pattern Demo***\n");
+        SubjectInterface Sc_Subject = new Science();
+        SubjectInterface Ar_subject = new Arts();
+
+        IteratorInterface Sc_iterator = Sc_Subject.createIterator();
+        IteratorInterface Ar_iterator = Ar_subject.createIterator();
+
+        System.out.println("\nScience subjects : " ) ;
+        print(Sc_iterator);
+
+
+        System.out.println("\nARts subjects");
+        print(Ar_iterator);
 
 
 
