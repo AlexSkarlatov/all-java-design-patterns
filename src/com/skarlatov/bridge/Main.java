6f8a3d763c83e2b6f00b81b809efe0419deceb48
alexs <alexskarlatov@gmail.com>
2021-02-05 16:36:52 -0500

    adding bridge pattern to my repository

---
+++ src/com/skarlatov/bridge/Main.java
@@ -0,0 +1,14 @@
+package com.skarlatov.bridge;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Vehicle v1 = new Car(new Produce(),new Assemble());
+        v1.manufacture();
+
+
+        Vehicle v2 = new Bike(new Produce(),new Assemble());
+        v2.manufacture();
+    }
+}
