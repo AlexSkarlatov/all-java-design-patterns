9b96254b650c9fc27e567ae6390c66d0d0afd059
alexs <alexskarlatov@gmail.com>
2021-02-05 16:43:49 -0500

    due to time constraints we will not be doing it right now     the branches are so poorly organized it might be easier to just redo the 3 all over again and get a refresher     on how it work anyway.

---
+++ src/com/skarlatov/builder/factory/prototype/Main.java
@@ -0,0 +1,18 @@
+package com.skarlatov.builder.factory.prototype;
+
+public class Main {
+    //TODO must port the originals to this singular location they all exist in this tertiary repository that
+    //is poorly organized
+    //===========================
+    //https://github.com/AlexSkarlatov/analysis-and-notes-on-design-patterns/tree/prototype-implementation-notes
+    //===========================
+    /*
+    please use the above url for this porting purpose. due to time constraints we will not be doing it right now
+    the branches are so poorly organized it might be easier to just redo the 3 all over again and get a refresher
+    on how it work anyway.
+     */
+
+
+
+
+}
